12f834a202bec6fd27fcfb192c1e1fca0b3a8e07
Hillary Reis <hillary.reis2002@gmail.com>
2021-03-03 15:37:35 -0300

    Criando models e aprofundando em callback function

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,12 +1,10 @@
 const { age, graduation, date } = require ('../../lib/utils');
-const db = require ('../../config/db');
+const Teacher = require ('../models/Teacher');
 
 module.exports = {
   index (req, res) {
-    db.query(`SELECT * FROM teachers`, function (err, results) {
-      if (err) return res.send ("Database Error!");
-
-      return res.render ("teachers/index", { teachers: results.rows });
+    Teacher.all (function (teachers) {
+      return res.render ('teachers/index', { teachers });
     });
   },
   create (req, res) {
@@ -21,35 +19,9 @@ module.exports = {
       };
     };
 
-    const query = `
-      INSERT INTO teachers (
-        avatar_url,
-        name,
-        birth_date,
-        education_level,
-        class_type,
-        subjects_taught,
-        created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7)
-      RETURNING id
-    `
-
-    const values = [
-      req.body.avatar_url,
-      req.body.name,
-      date(req.body.birth_date).iso,
-      req.body.education_level,
-      req.body.class_type,
-      req.body.subjects_taught,
-      date(Date.now()).iso,
-    ];
-
-    db.query(query, values, function (err, results) {
-      if (err) return res.send ("Database Error!");
-
-      return res.redirect (`/teachers/${results.rows[0].id}`);
+    Teacher.create (req.body, function (teacher) {
+      return res.redirect (`/teachers/${teacher.id}`);
     });
-
   },
   show (req, res) {
     return res.render ("teachers/show");
---
+++ src/app/models/Teacher.js
@@ -0,0 +1,45 @@
+const db = require ('../../config/db');
+const { age, graduation, date } = require ('../../lib/utils');
+
+module.exports = {
+  all (callback) {
+    db.query(`SELECT * FROM teachers`, function (err, results) {
+      if (err) return res.send ("Database Error!");
+
+      callback(results.rows);
+    });
+  },
+  create (data, callback) {
+    const query = `
+      INSERT INTO teachers (
+        avatar_url,
+        name,
+        birth_date,
+        education_level,
+        class_type,
+        subjects_taught,
+        created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING id
+    `
+
+    const values = [
+      data.avatar_url,
+      data.name,
+      date(data.birth_date).iso,
+      data.education_level,
+      data.class_type,
+      data.subjects_taught,
+      date(Date.now()).iso,
+    ];
+
+    db.query(query, values, function (err, results) {
+      if (err) return res.send ("Database Error!");
+
+      callback(results.rows[0]);
+    });
+  },
+  find () {},
+  update () {},
+  delete () {},
+}
