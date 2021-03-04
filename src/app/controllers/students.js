75866aebb9b068e0774433427dcf84c2949c98f5
Hillary Reis <hillary.reis2002@gmail.com>
2021-03-04 15:17:30 -0300

    Ajustando o controller de estudantes com base no dos professores

--- src/app/controllers/students.js
+++ src/app/controllers/students.js
@@ -1,8 +1,11 @@
 const { age, graduation, date } = require ('../../lib/utils');
+const Student = require ('../models/Student');
 
 module.exports = {
   index (req, res) {
-    return res.render ("students/index");
+    Student.all (function (students) {
+      return res.render ('students/index', { students });
+    });
   },
   create (req, res) {
     return res.render ('students/create');
@@ -16,15 +19,28 @@ module.exports = {
       };
     };
 
-    return
-  
-    //return res.redirect (`/students/${id}`);
+    Student.create (req.body, function (student) {
+      return res.redirect (`/students/${student.id}`);
+    });
   },
   show (req, res) {
-    return res.render ("students/show");
+    Student.find(req.params.id, function (student) {
+      if (!student) return res.send ("student not found!");
+
+      student.birth_date = date (student.birth_date).birth_dateDay;
+      student.grade = grade (student.schoolYear);
+
+      return res.render ('students/show', { student });
+    });
   },
   edit (req, res) {
-    return res.render ('students/edit');
+    Student.find(req.params.id, function (student) {
+      if (!student) return res.send ("student not found!");
+
+      student.birth_date = date(student.birth_date).iso;
+
+      return res.render ('students/edit', { student });
+    });
   },
   put (req, res) {
     const keys = Object.keys(req.body);
@@ -35,10 +51,13 @@ module.exports = {
       };
     };
 
-    return
-    //return res.redirect(`/students/${ id }`);
+    Student.update(req.body, function () {
+      return res.redirect(`/students/${ req.body.id }`);
+    });
   },
   delete (req, res) {
-    return res.redirect ("/students");
+    Student.delete(req.body.id, function () {
+      return res.redirect ('/students');
+    });
   },
 }
